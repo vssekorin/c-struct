c5714c3ce97e298957f469e2eac28b5dfbfe049e
VsSekorin <vssekorin@gmail.com>
2020-03-12 01:07:29 +0300

    Integer Linked List

---
+++ ilist.c
@@ -0,0 +1,692 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "ilist.h"
+
+int is_empty(IList *list) {
+    return list->size == 0;
+}
+
+int is_not_empty(IList *list) {
+    return list->size != 0;
+}
+
+IList* empty_list() {
+    IList *list = (IList*) malloc(sizeof(IList));
+    list->size = 0;
+    return list;
+}
+
+IList* single_list_of(int value) {
+    Node *node = (Node*) malloc(sizeof(Node));
+    node->value = value;
+    node->next = NULL;
+    IList *list = (IList*) malloc(sizeof(IList));
+    list->first = node;
+    list->last = node;
+    list->size = 1;
+    return list;
+}
+
+IList* list_of(int count, ...) {
+    va_list items;
+    va_start(items, count);
+    IList *list = (IList*) malloc(sizeof(IList));
+    for (int i = 0; i < count; i++) {
+        push_back(list, va_arg(items, int));
+    }
+    va_end(items);
+    return list;
+}
+
+IList* range(int first, int last) {
+    return range_step(first, last, first < last ? 1 : -1);
+}
+
+IList* range_step(int first, int last, int step) {
+    IList *list = empty_list();
+    if (step > 0) {
+        for (int i = first; i <= last; i += step) {
+            push_back(list, i);
+        }
+    } else {
+        for (int i = first; i >= last; i += step) {
+            push_back(list, i);
+        }
+    }
+    return list;
+}
+
+IList* range_ex(int first, int last) {
+    return range_step_ex(first, last, first < last ? 1 : -1);
+}
+
+IList* range_step_ex(int first, int last, int step) {
+    IList *list = empty_list();
+    if (step > 0) {
+        for (int i = first; i < last; i += step) {
+            push_back(list, i);
+        }
+    } else {
+        for (int i = first; i > last; i += step) {
+            push_back(list, i);
+        }
+    }
+    return list;
+}
+
+IList* generate_n(int start, i_func op, int count) {
+    IList *list = empty_list();
+    for (int cur = start, i = 0; i < count; cur = op(cur), i++) {
+        push_back(list, cur);
+    }
+    return list;
+}
+
+IList* generate_while(int start, i_func op, i_func cond) {
+    IList *list = empty_list();
+    for (int cur = start; cond(cur); cur = op(cur)) {
+        push_back(list, cur);
+    }
+    return list;
+}
+
+IList* repeat(int value, int count) {
+    IList *list = empty_list();
+    for (int i = 0; i < count; i++) {
+        push(list, value);
+    }
+    return list;
+}
+
+void delete_list(IList **list) {
+    if (is_not_empty(*list)) {
+        Node *node;
+        while ((*list)->first->next) {
+            node = (*list)->first;
+            (*list)->first = (*list)->first->next;
+            free(node);
+        }
+        free((*list)->first);
+    }
+    free(*list);
+}
+
+int equals(IList *fst, IList *snd) {
+    if (fst->size != snd->size) {
+        return 0;
+    }
+    for (Node *c1 = fst->first, *c2 = snd->first; c1->next || c2->next; c1 = c1->next, c2 = c2->next) {
+        if (c1 == NULL && c2 != NULL || c1 != NULL && c2 == NULL || c1->value != c2->value) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int get_size(IList *list) {
+    return list->size;
+}
+
+IList* push(IList* list, int value) {
+    Node *node = (Node*) malloc(sizeof(Node));
+    node->value = value;
+    if (is_empty(list)) {
+        list->last = node;
+    } else {
+        node->next = list->first;
+    }
+    list->first = node;
+    list->size++;
+}
+
+IList* push_back(IList* list, int value) {
+    Node *node = (Node*) malloc(sizeof(Node));
+    node->value = value;
+    node->next = NULL;
+    if (is_empty(list)) {
+        list->first = node;
+    } else {
+        list->last->next = node;
+    }
+    list->last = node;
+    list->size++;
+    return list;
+}
+
+IList* insert(IList *list, int pos, int value) {
+    if (pos == 0) {
+        push(list, value);
+    } else if (pos == list->size) {
+        push_back(list, value);
+    } else {
+        Node *node = (Node*) malloc(sizeof(Node));
+        node->value = value;
+        Node *prev = get_node(list, pos - 2);
+        node->next = prev->next;
+        prev->next = node;
+        list->size++;
+    }
+    return list;
+}
+
+int pop(IList *list) {
+    int value = list->first->value;
+    drop(list);
+    return value;
+}
+
+int pop_back(IList *list) {
+    int value = list->last->value;
+    drop_back(list);
+    return value;
+}
+
+IList* delete(IList *list, int pos) {
+    Node *prev = get_node(list, pos - 1);
+    Node *del = prev->next;
+    prev->next = del->next;
+    free(del);
+    return list;
+}
+
+IList* delete_item(IList *list, int value) {
+    int index = index_of(list, value);
+    while (index != -1) {
+        delete(list, index);
+        index = index_of(list, value);
+    }
+    return list;
+}
+
+IList* drop(IList *list) {
+    Node *node = list->first;
+    if (list->size == 1) {
+        list->last = NULL;
+    }
+    list->first = node->next;
+    list->size--;
+    free(node);
+    return list;
+}
+
+IList* drop_back(IList *list) {
+    Node *node = list->last;
+    if (list->size == 1) {
+        list->first = NULL;
+        list->last = NULL;
+    } else {
+        Node *prev = get_node(list, list->size - 2);
+        prev->next = NULL;
+        list->last = prev;
+    }
+    list->size--;
+    free(node);
+    return list;
+}
+
+IList* drop_n(IList *list, int n) {
+    for (int i = 0; i < n && i < list->size - 1; i++) {
+        drop(list);
+    }
+    return list;
+}
+
+IList* drop_back_n(IList *list, int n) {
+    for (int i = 0; i < n && i < list->size - 1; i++) {
+        drop_back(list);
+    }
+    return list;
+}
+
+IList* drop_while(IList *list, i_func pred) {
+    while (list->first && pred(list->first->value)) {
+        drop(list);
+    }
+    return list;
+}
+
+IList* drop_back_while(IList *list, i_func pred) {
+    while (list->last && pred(list->last->value)) {
+        drop_back(list);
+    }
+    return list;
+}
+
+IList* update(IList *list, int pos, int value) {
+    get_node(list, pos)->value = value;
+    return list;
+}
+
+IList* sublist(IList *list, int start, int end) {
+    for (int i = 0; i < list->size - end; i++) {
+        drop_back(list);
+    }
+    for (int i = 0; i < start; i++) {
+        drop(list);
+    }
+    return list;
+}
+
+IList* from_array(int size, int *arr) {
+    IList *result = empty_list();
+    if (arr == NULL || size == 0) {
+        return result;
+    }
+    for (int i = 0; i < size; i++) {
+        push_back(result, arr[i]);
+    }
+    return result;
+}
+
+int* to_array(IList *list) {
+    int count = list->size;
+    int *arr = (int*) malloc(count * sizeof(int));
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        arr[--count] = cur->value;
+    }
+    return arr;
+}
+
+void foreach(IList *list, void (*op)(int)) {
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        op(cur->value);
+    }
+}
+
+void print_list(IList *list) {
+    printf("IList %p : [", list);
+    if (is_not_empty(list)) {
+        for (Node *cur = list->first; cur, cur->next; cur = cur->next) {
+            printf("%d, ", cur->value);
+        }
+        printf("%d", list->last->value);
+    }
+    printf("]\n");
+}
+
+IList* reverse(IList *list) {
+    IList *result = empty_list();
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        push(result, cur->value);
+    }
+    return result;
+}
+
+Node* get_first_node(IList *list) {
+    return list->first;
+}
+
+Node* get_last_node(IList *list) {
+    return list->last;
+}
+
+Node* get_node(IList *list, int index) {
+    if (index == 0) {
+        return get_first_node(list);
+    } else if (index == list->size - 1) {
+        return get_last_node(list);
+    } else {
+        Node* cur = list->first;
+        for (int i = 0; cur && i < index; cur = cur->next, i++);
+        return cur;
+    }
+}
+
+int get_first(IList *list) {
+    return list->first->value;
+}
+
+int get_last(IList *list) {
+    return list->last->value;
+}
+
+int get(IList *list, int index) {
+    return get_node(list, index)->value;
+}
+
+int find(IList *list, i_func pred) {
+    return find_or(list, pred, 0);
+}
+
+int find_not(IList *list, i_func pred) {
+    return find_not_or(list, pred, 0);
+}
+
+int find_or(IList *list, i_func pred, int default_value) {
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (pred(cur->value)) {
+            return cur->value;
+        }
+    }
+    return default_value;
+}
+
+int find_not_or(IList *list, i_func pred, int default_value) {
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (!pred(cur->value)) {
+            return cur->value;
+        }
+    }
+    return default_value;
+}
+
+int index_of(IList *list, int item) {
+    int index = 0;
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (item == cur->value) {
+            return index;
+        }
+        index++;
+    }
+    return -1;
+}
+
+int last_index_of(IList *list, int item) {
+    int index = -1;
+    int i = 0;
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (item == cur->value) {
+            index = i;
+        }
+        i++;
+    }
+    return index;
+}
+
+IList* clone(IList *list) {
+    IList *result = empty_list();
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        push_back(result, cur->value);
+    }
+    return result;
+}
+
+IList* map(IList *list, i_func op) {
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        cur->value = op(cur->value);
+    }
+    return list;
+}
+
+IList* filter(IList *list, i_func pred) {
+    IList *result = empty_list();
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (pred(cur->value)) {
+            push_back(result, cur->value);
+        }
+    }
+    return result;
+}
+
+IList* filter_not(IList *list, i_func pred) {
+    IList *result = empty_list();
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (!pred(cur->value)) {
+            push_back(result, cur->value);
+        }
+    }
+    return result;
+}
+
+IList* add_all(IList *list, IList *elems) {
+    for (Node *cur = elems->first; cur; cur = cur->next) {
+        push_back(list, cur->value);
+    }
+    return list;
+}
+
+int contains(IList *list, int value) {
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (cur->value == value) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int fold_left(int init, IList *list, i_bifunc op) {
+    int acc = init;
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        acc = op(acc, cur->value);
+    }
+    return acc;
+}
+
+int reduce_left(IList *list, i_bifunc op) {
+    if (is_empty(list)) {
+        return 0;
+    } else if (list->size == 1) {
+        return list->first->value;
+    } else {
+        Node *first = list->first;
+        list->first = list->first->next;
+        int result = fold_left(first->value, list, op);
+        list->first = first;
+        return result;
+    }
+}
+
+int fold_right(int init, IList *list, i_bifunc op) {
+    int acc = init;
+    IList *rev = reverse(list);
+    for (Node *cur = rev->first; cur; cur = cur->next) {
+        acc = op(cur->value, acc);
+    }
+    delete_list(&rev);
+    return acc;
+}
+
+int reduce_right(IList *list, i_bifunc op) {
+    if (is_empty(list)) {
+        return 0;
+    } else if (list->size == 1) {
+        return list->first->value;
+    } else {
+        Node *prev = get_node(list, list->size - 2);
+        Node *last = list->last;
+        prev->next = NULL;
+        list->last = prev;
+        int result = fold_right(last->value, list, op);
+        prev->next = last;
+        list->last = last;
+        return result;
+    }
+}
+
+IList* take(IList *list, int n) {
+    IList *result = empty_list();
+    Node *cur = list->first;
+    for (int i = 0; i < n && cur; i++, cur = cur->next) {
+        push_back(result, cur->value);
+    }
+    return result;
+}
+
+IList* take_while(IList *list, i_func pred) {
+    IList *result = empty_list();
+    for (Node *cur = list->first; cur && pred(cur->value); cur = cur->next) {
+        push_back(result, cur->value);
+    }
+    return result;
+}
+
+IList* take_right(IList *list, int n) {
+    IList *result = empty_list();
+    int i = 0;
+    for (Node *cur = list->first; cur; cur = cur->next, i++) {
+        if (i >= list->size - n) {
+            push_back(result, cur->value);
+        }
+    }
+    return result;
+}
+
+IList* take_right_while(IList *list, i_func pred) {
+    IList *rev = reverse(list);
+    IList *tw = take_while(rev, pred);
+    IList *result = take_right(list, tw->size);
+    delete_list(&rev);
+    delete_list(&tw);
+    return result;
+}
+
+IList* slice(IList *list, int start, int end) {
+    if (start == 0) {
+        return take(list, end + 1);
+    } else if (end == list->size - 1) {
+        return take_right(list, end - start + 1);
+    } else {
+        IList *result = empty_list();
+        int i = 0;
+        for (Node *cur = list->first; cur; cur = cur->next, i++) {
+            if (i >= start && i <= end) {
+                push_back(result, cur->value);
+            }
+        }
+        return result;
+    }
+}
+
+int forall(IList *list, i_func pred) {
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (!pred(cur->value)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int exists(IList *list, i_func pred) {
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (pred(cur->value)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int count(IList *list, i_func pred) {
+    int count = 0;
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (pred(cur->value)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+IList* unique(IList *list) {
+    IList *result = empty_list();
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (!contains(result, cur->value)) {
+            push_back(result, cur->value);
+        }
+    }
+    return result;
+}
+
+IList* distinct(IList *list) {
+    IList *buffer = empty_list();
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (contains(buffer, cur->value)) {
+            delete(list, index_of(list, cur->value));
+        } else {
+            push(buffer, cur->value);
+        }
+    }
+    delete_list(&buffer);
+    return list;
+}
+
+IList* swap(IList *list, int i, int j) {
+    Node *ni = get_node(list, i);
+    Node *nj = get_node(list, j);
+    int vi = ni->value;
+    ni->value = nj->value;
+    nj->value = vi;
+    return list;
+}
+
+int is_prefix(IList *list, IList *prefix) {
+    if (is_empty(prefix)) {
+        return 1;
+    }
+    if (is_empty(list)) {
+        return 0;
+    }
+    for (Node *l = list->first, *p = prefix->first; p; l = l->next, p = p->next) {
+        if (!l || l->value != p->value) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int is_suffix(IList *list, IList *suffix) {
+    IList *rl = reverse(list);
+    IList *rs = reverse(suffix);
+    int result = is_prefix(rl, rs);
+    delete_list(&rl);
+    delete_list(&rs);
+    return result;
+}
+
+int is_sublist(IList *list, IList *sublist) {
+    if (is_empty(sublist)) {
+        return 1;
+    }
+    if (is_empty(list) || list->size < sublist->size) {
+        return 0;
+    }
+    IList *copy = clone(list);
+    do {
+        drop_n(copy, index_of(copy, sublist->first->value));
+        if (is_prefix(copy, sublist)) {
+            delete_list(&copy);
+            return 1;
+        }
+    } while (copy->size > sublist->size);
+    delete_list(&copy);
+    return 0;
+}
+
+IList* intersperse(IList *list, int value) {
+    IList *result = empty_list();
+    if (is_not_empty(list)) {
+        for (Node *cur = list->first; cur->next; cur = cur->next) {
+            push_back(result, cur->value);
+            push_back(result, value);
+        }
+        push_back(result, list->last->value);
+    }
+    return result;
+}
+
+int max(IList *list) {
+    int max = list->first->value;
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (cur->value > max) {
+            max = cur->value;
+        }
+    }
+    return max;
+}
+
+int min(IList *list) {
+    int min = list->first->value;
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        if (cur->value < min) {
+            min = cur->value;
+        }
+    }
+    return min;
+}
+
+int sum(IList *list) {
+    int sum = 0;
+    for (Node *cur = list->first; cur; cur = cur->next) {
+        sum += cur->value;
+    }
+    return sum;
+}
---
+++ ilist.h
@@ -0,0 +1,260 @@
+#ifndef ILIST_H_
+#define ILIST_H_
+
+typedef int (*i_func)(int);
+typedef int (*i_bifunc)(int, int);
+
+/* Integer Linked List */
+
+typedef struct Node {
+    int value;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    Node *first;
+    Node *last;
+    int size;
+} IList;
+
+/* Returns true if this list contains some elements. */
+extern int is_empty(IList*);
+
+/* Returns true if this list contains no elements. */
+extern int is_not_empty(IList*);
+
+/* Returns a empty list. */
+extern IList* empty_list();
+
+/* Returns a list containing only the specified object. */
+extern IList* single_list_of();
+
+/* Returns a list containing an arbitrary number of elements. */
+extern IList* list_of(int, ...);
+
+/* Returns a sequential ordered IList from start (inclusive)
+    to end (inclusive) by an incremental step of 1 (or -1). */
+extern IList* range(int, int);
+
+/* Returns a sequential ordered IList from start (inclusive)
+    to end (inclusive) by a specified step. */
+extern IList* range_step(int, int, int);
+
+/* Returns a sequential ordered IList from start (inclusive)
+    to end (exclusive) by an incremental step of 1 (or -1). */
+extern IList* range_ex(int, int);
+
+/* Returns a sequential ordered IList from start (inclusive)
+    to end (exclusive) by a specified step. */
+extern IList* range_step_ex(int, int, int);
+
+/* Returns a list of n elements produced by iterative application
+    of a function to an initial element. */
+extern IList* generate_n(int, i_func, int);
+
+/* Returns a list produced by iterative application of a function
+    to an initial element while the condition is satisfied. */
+extern IList* generate_while(int, i_func, i_func);
+
+/* Returns a list consisting of n copies of the specified object. */
+extern IList* repeat(int, int);
+
+/* Delete the list. */
+extern void delete_list(IList**);
+
+/* Returns true if the arguments are equal to each other and false otherwise. */
+extern int equals(IList*, IList*);
+
+/* Returns a size of the list. */
+extern int get_size(IList*);
+
+/* [Mutator] Pushes an element onto the stack represented by this list.
+    (Inserts the specified element at the beginning of this list.) */
+extern IList* push(IList*, int);
+
+/* [Mutator] Appends the specified element to the end of this list. */
+extern IList* push_back(IList*, int);
+
+/* [Mutator] Inserts the specified element at the position of this list. */
+extern IList* insert(IList*, int, int);
+
+/* [Mutator] Pops an element from the stack represented by this list.
+    (Removes and returns the first element from this list.) */
+extern int pop(IList*);
+
+/* [Mutator] Removes and returns the last element from this list. */
+extern int pop_back(IList*);
+
+/* [Mutator] Removes the element at the specified position in this list. */
+extern IList* delete(IList*, int);
+
+/* [Mutator] Removes all occurrences of the specified element from this list. */
+extern IList* delete_item(IList*, int);
+
+/* [Mutator] Removes the first element from this list. */
+extern IList* drop(IList*);
+
+/* [Mutator] Removes the last element from this list. */
+extern IList* drop_back(IList*);
+
+/* [Mutator] Removes first n elements from this list. */
+extern IList* drop_n(IList*, int);
+
+/* [Mutator] Removes last n elements from this list. */
+extern IList* drop_back_n(IList*, int);
+
+/* [Mutator] Removes first elements from this list while the condition is satisfied. */
+extern IList* drop_while(IList*, i_func);
+
+/* [Mutator] Removes last elements from this list while the condition is satisfied. */
+extern IList* drop_back_while(IList*, i_func);
+
+/* [Mutator] Replaces the element at the specified position in this list
+    with the specified value. */
+extern IList* update(IList*, int, int);
+
+/* [Mutator] Returns sublist. */
+extern IList* sublist(IList*, int, int);
+
+/* Returns a list containing elements of the array. */
+extern IList* from_array(int, int*);
+
+/* Returns an array containing elements of the list. */
+extern int* to_array(IList*);
+
+/* Performs the given action for each element of the list. */
+extern void foreach(IList*, void (*op)(int));
+
+/* Print the list. */
+extern void print_list(IList*);
+
+/* Returns new list with elements in reversed order. */
+extern IList* reverse(IList*);
+
+/* Returns a first node of the list. */
+extern Node* get_first_node(IList*);
+
+/* Returns a last node of the list. */
+extern Node* get_last_node(IList*);
+
+/* Returns a node of the list at the specified position. */
+extern Node* get_node(IList*, int);
+
+/* Returns a first value of the list. */
+extern int get_first(IList*);
+
+/* Returns a last value of the list. */
+extern int get_last(IList*);
+
+/* Returns a value of the list at the specified position. */
+extern int get(IList*, int);
+
+/* Finds the first element of the list a predicate. */
+extern int find(IList*, i_func);
+
+/* Finds the first element of the list a negative predicate. */
+extern int find_not(IList*, i_func);
+
+/* Finds the first element of the list a predicate or returns default value. */
+extern int find_or(IList*, i_func, int);
+
+/* Finds the first element of the list a negative predicate or returns default value. */
+extern int find_not_or(IList*, i_func, int);
+
+/* Returns the index of the first occurrence of the specified element in this list,
+    or -1 if this list does not contain the element. */
+extern int index_of(IList*, int);
+
+/* Returns the index of the last occurrence of the specified element in this list,
+    or -1 if this list does not contain the element. */
+extern int last_index_of(IList*, int);
+
+/* Returns a copy of this. */
+extern IList* clone(IList*);
+
+/* Returns a list consisting of the results of applying the given function
+    to the elements of this list. */
+extern IList* map(IList*, i_func);
+
+/* Returns a list consisting of the elements of this list that match the given predicate. */
+extern IList* filter(IList*, i_func);
+
+/* Returns a list consisting of the elements of this list that
+    match the negation given predicate. */
+extern IList* filter_not(IList*, i_func);
+
+/* Appends all of the elements in the second collection to the end of first list. */
+extern IList* add_all(IList*, IList*);
+
+/* Returns true if the list contains the specified element. */
+extern int contains(IList*, int);
+
+/* Applies a binary operator to a start value and all elements of the list, going left to right. */
+extern int fold_left(int, IList*, i_bifunc);
+
+/* Applies a binary operator to all elements of the list, going left to right. */
+extern int reduce_left(IList*, i_bifunc);
+
+/* Applies a binary operator to all elements of the list and a start value, going right to left. */
+extern int fold_right(int, IList*, i_bifunc);
+
+/* Applies a binary operator to all elements of the list, going right to left. */
+extern int reduce_right(IList*, i_bifunc);
+
+/* Selects first n elements. */
+extern IList* take(IList*, int);
+
+/* Takes longest prefix of elements that satisfy a predicate. */
+extern IList* take_while(IList*, i_func);
+
+/* Selects last n elements. */
+extern IList* take_right(IList*, int);
+
+/* Takes longest suffix of elements that satisfy a predicate. */
+extern IList* take_right_while(IList*, i_func);
+
+/* Returns a list containing the elements greater than or equal to index from extending up
+    to index until of the list. */
+extern IList* slice(IList*, int, int);
+
+/* Tests whether a predicate holds for all elements of the list. */
+extern int forall(IList*, i_func);
+
+/* Tests whether a predicate holds for at least one element of the list. */
+extern int exists(IList*, i_func);
+
+/* Counts the number of elements in the list which satisfy a predicate. */
+extern int count(IList*, i_func);
+
+/* Builds a new list from the list without any duplicate elements. */
+extern IList* unique(IList*);
+
+/* [Mutator] Returns a list consisting only of the distinct elements (according to ==). */
+extern IList* distinct(IList*);
+
+/* [Mutator] Swap values of two elements in the list. */
+extern IList* swap(IList*, int, int);
+
+/* Returns true iff the first list is a prefix of the second. */
+extern int is_prefix(IList*, IList*);
+
+/* Returns true iff the first list is a suffix of the second. */
+extern int is_suffix(IList*, IList*);
+
+/* Returns true iff the first list is a sublist of the second. */
+extern int is_sublist(IList*, IList*);
+
+/* Returns a new list containing elements that `interspersed`
+    by the element between the elements of the list. */
+extern IList* intersperse(IList*, int);
+
+/* Finds the largest element. */
+extern int max(IList*);
+
+/* Finds the smallest element. */
+extern int min(IList*);
+
+/* Sums up the elements of the list. */
+extern int sum(IList*);
+
+#endif
